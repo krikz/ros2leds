83ecffb0cecb23bd30100fb251a26d5eb38ad635
Your Name <you@example.com>
2025-08-24 12:47:04 +0300

    Lounch driver and compositor together

--- led_matrix_compositor/launch/led_matrix_compositor_launch.py
+++ led_matrix_compositor/launch/led_matrix_compositor_launch.py
@@ -4,19 +4,36 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    # Путь к конфигурационному файлу
-    config_path = PathJoinSubstitution([
+    # Путь к конфигурационному файлу композитора
+    compositor_config_path = PathJoinSubstitution([
         FindPackageShare('led_matrix_compositor'),
         'config',
         'led_matrix_compositor.yaml'
     ])
     
+    # Путь к конфигурационному файлу драйвера
+    driver_config_path = PathJoinSubstitution([
+        FindPackageShare('led_matrix_driver'),
+        'config',
+        'led_matrix_driver.yaml'
+    ])
+    
     return LaunchDescription([
+        # Запускаем драйвер матрицы
+        Node(
+            package='led_matrix_driver',
+            executable='led_matrix_driver',
+            name='led_matrix_driver',
+            output='screen',
+            parameters=[driver_config_path]
+        ),
+        
+        # Запускаем композитор матрицы
         Node(
             package='led_matrix_compositor',
             executable='led_matrix_compositor',
             name='led_matrix_compositor',
             output='screen',
-            parameters=[config_path]
+            parameters=[compositor_config_path]
         )
     ])
